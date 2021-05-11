741b4360b8807861a3fa4c69fc6aafc9a6695bba
alansenairj <alansenairj@gmail.com>
2021-05-11 16:49:14 -0300

    Update tplinkreboot v3.py

--- tplinkreboot v3.py
+++ tplinkreboot v3.py
@@ -1,6 +1,9 @@
+#teste login 1 ok
 import requests
 r = requests.get('http://192.168.129.1' , auth=('admin', 'admin'))
 r.text
+
+#teste login 2 ok
 
 import requests
 class MyAuth(requests.auth.AuthBase):
@@ -12,7 +15,11 @@ url = 'http://192.168.129.1'
 requests.get(url, auth=MyAuth())
 print(r)
 
+#teste login 3 ok
+
 payload = { 'username': 'admin', 'password': 'admin' }
 with requests.Session() as s:
   p = s.post('http://192.168.129.1/', data=payload) 
   p = s.get('http://192.168.129.1/userRpm/SysRebootRpm.htm')
+
+  #não sei como pegar a confirmação de rebbot que o scrit gera para executar a solicitação
